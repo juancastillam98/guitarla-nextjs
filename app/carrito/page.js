d032aa229adbb7a69797dd8769dd27076e66d1a3
juancastillam98 <programacion@prodigia.com>
2023-11-09 20:55:35 +0100

    ultima modificación

--- app/carrito/page.js
+++ app/carrito/page.js
@@ -1,62 +1,64 @@
-"use client"
-import {useEffect, useState} from "react";
+"use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import {Context} from "../context"
+import { Context } from "../context";
 import { useContext } from "react";
-import styles from '@/styles/carrito.module.css'
-/*export const metadata={
-    title: 'Carrito',
-    description: 'Compra'
-}*/
+import styles from "@/styles/carrito.module.css";
+
 export default function Carrito() {
     const context = useContext(Context);
-
-    const { carrito, actualizarCantidad, eliminarProducto } = context;
-    const [total, setTotal] = useState(0);
-
-    //vamos a calcular el total mediante un acumulador (reduce)
-    useEffect(() =>{
-        const calculoTotal = carrito.reduce((acumulado, elementoIterado)=>acumulado + (elementoIterado.cantidad * elementoIterado.precio), 0)//0 es el valor inicial
-        setTotal(calculoTotal)
-        /* esto de arriba es lo mismo que cuando en un bucle convencional -->
-       totalCarrito=0;
-        for (let i = 0; i < carrito.length; i){
-            totalCarrito += carrito[i].precio * carrito[i].cantidad;
-        }
-        setTotal(totalCarrito)
-        */
-    }, [carrito])//lo vamos a meter en un useEffect para que compruebe el total cada vez que ocurra algún cambio
-
 
     if (!context) {
         // Manejar el caso cuando el contexto es nulo (por ejemplo, aún no está listo)
         return <div>Cargando...</div>;
     }
 
-    return(
+    const { carrito, actualizarCantidad, eliminarProducto } = context;
+    const [total, setTotal] = useState(0);
+
+    //vamos a calcular el total mediante un acumulador (reduce)
+    useEffect(() => {
+        const calculoTotal = carrito.reduce(
+            (acumulado, elementoIterado) =>
+                acumulado + elementoIterado.cantidad * elementoIterado.precio,
+            0
+        );
+        setTotal(calculoTotal);
+    }, [carrito]);
+
+    return (
         <>
             <main className={"contenedor"}>
                 <h1 className={"heading"}>Carrito</h1>
                 <div className={styles.contenido}>
                     <div className={styles.carrito}>
                         <h2>Artículos</h2>
-                        {carrito.length === 0 ? "Carrito Vacío" : (
-                            carrito.map((producto)=>(
+                        {carrito.length === 0 ? (
+                            "Carrito Vacío"
+                        ) : (
+                            carrito.map((producto) => (
                                 <div key={producto.id} className={styles.producto}>
                                     <div>
-                                        <Image src={producto.imagen} width={250} height={480} alt={producto.nombre}/>
+                                        <Image
+                                            src={producto.imagen}
+                                            width={250}
+                                            height={480}
+                                            alt={producto.nombre}
+                                        />
                                     </div>
                                     <div>
                                         <p className={styles.nombre}>{producto.nombre}</p>
                                         <div className={styles.cantidad}>
                                             <p>Cantidad:</p>
-                                            <select className={styles.select}
-                                                    onChange={ (e)=>
-                                                        actualizarCantidad({
-                                                            id: producto.id,
-                                                            cantidad: e.target.value
-                                                        })}
-                                                    value={producto.cantidad}
+                                            <select
+                                                className={styles.select}
+                                                onChange={(e) =>
+                                                    actualizarCantidad({
+                                                        id: producto.id,
+                                                        cantidad: e.target.value,
+                                                    })
+                                                }
+                                                value={producto.cantidad}
                                             >
                                                 <option value={1}>1</option>
                                                 <option value={2}>2</option>
@@ -65,18 +67,21 @@ export default function Carrito() {
                                                 <option value={5}>5</option>
                                             </select>
                                         </div>
-                                        <p className={styles.precio}>$<span>{producto.precio}</span></p>
-                                        <p className={styles.subtotal}>$<span>{producto.cantidad * producto.precio}</span></p>
+                                        <p className={styles.precio}>
+                                            $<span>{producto.precio}</span>
+                                        </p>
+                                        <p className={styles.subtotal}>
+                                            $<span>{producto.cantidad * producto.precio}</span>
+                                        </p>
                                     </div>
                                     <button
                                         className={styles.eliminar}
                                         type={"button"}
-                                        onClick={()=>eliminarProducto(producto.id)}
+                                        onClick={() => eliminarProducto(producto.id)}
                                     >
                                         X
                                     </button>
                                 </div>
-
                             ))
                         )}
                     </div>
@@ -87,5 +92,5 @@ export default function Carrito() {
                 </div>
             </main>
         </>
-    )
+    );
 }
