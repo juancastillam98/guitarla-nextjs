b26e5737f159bbb7ee60811f3667565ad547735a
juancastillam98 <programacion@prodigia.com>
2023-11-09 20:17:10 +0100

    solucionado problema useContext

--- app/carrito/page.js
+++ app/carrito/page.js
@@ -10,12 +10,13 @@ import styles from '@/styles/carrito.module.css'
 }*/
 export default function Carrito() {
     const context = useContext(Context);
+    const [total, setTotal] = useState(0);
     if (!context) {
         // Manejar el caso cuando el contexto es nulo (por ejemplo, aún no está listo)
         return <div>Cargando...</div>;
     }
     const { carrito, actualizarCantidad, eliminarProducto } = context;
-    const [total, setTotal] = useState(0);
+
 
     //vamos a calcular el total mediante un acumulador (reduce)
     useEffect(() =>{
