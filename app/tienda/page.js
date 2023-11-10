e232e0672d7a091bb29d9b5789c4a22b8f16e87a
juancastillam98 <programacion@prodigia.com>
2023-11-10 18:15:17 +0100

    solucion 5

--- app/tienda/page.js
+++ app/tienda/page.js
@@ -1,12 +1,11 @@
 import {Metadata} from "next";
 import {Guitarra} from "../../components/Guitarra";
-import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
 import styles from "../../styles/grid.module.css"
 export const metadata={
     title: "Tienda Virtual",
     description: "Tienda de guitarras"
 }
-export default async function Tienda(){//lo estoy extrayendo en getStaticProps
+export default async function Tienda(){
     const guitarras = await getGuitarras();
     return(
         <main className={"contenedor"}>
@@ -22,11 +21,8 @@ export default async function Tienda(){//lo estoy extrayendo en getStaticProps
     )
 }
 
-async function getGuitarras() {
+export async function getGuitarras() {
     const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
     const {data} = await respuesta.json();
-    if (!respuesta.ok) {
-        throw new Error('Failed to fetch data')
-    }
     return data
 }
